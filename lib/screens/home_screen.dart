fae4a11a2d6b8ca076928c2422990520ff82c3db
Mohamed855 <mohamedgedo56@yahoo.com>
2021-02-25 01:01:11 +0200

    done for now

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:picsStore/content.dart';
-import 'package:picsStore/screens/gallery.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
