e83ebbc1b4750b500266251a7e7d3f42661d9415
Mohamed855 <mohamedgedo56@yahoo.com>
2021-02-27 21:41:58 +0200

    upload

--- lib/global.dart
+++ lib/global.dart
@@ -2,10 +2,4 @@ int imgIndex = 0;
 
 double padding = 20.0, borderRadius = 8.0;
 
-List imgUrls = [
-  {
-    'name': 'Tree',
-    'src':
-        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg',
-  }
-];
+List imgUrls = [];
